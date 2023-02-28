37f7d4ad34573d9355522e350f1ff0a32baf7be5
Jurgita <jurgita.eicinaite@yahoo.com>
2023-02-28 19:01:57 +0200

    form onchange and onsubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import AddNewAcccount from './Components/AddNewAccount';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header"></header>
+            <header className="App-header">
+                <AddNewAcccount />
+            </header>
         </div>
     );
 }
---
+++ src/Components/AddNewAccount.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const AddNewAcccount = () => {
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const addNameHandler = e => {
+        setName(e.target.value);
+    };
+
+    const addLastNameHandler = e => {
+        setLastName(e.target.value);
+    };
+
+    const submitHandler = e => {
+        e.preventDefault();
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                id="name"
+                onChange={addNameHandler}
+            />
+            <label htmlFor="lastname">Last Name</label>
+            <input
+                type="text"
+                id="lastname"
+            />
+            <button
+                type="submit"
+                onChange={addLastNameHandler}>
+                Add Account
+            </button>
+        </form>
+    );
+};
+
+export default AddNewAcccount;
