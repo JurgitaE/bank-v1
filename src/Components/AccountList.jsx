2c713853a4a81252903c7664af9b31eb3288c08c
Jurgita <jurgita.eicinaite@yahoo.com>
2023-03-01 09:28:01 +0200

    account list deposit

--- src/Components/AccountList.jsx
+++ src/Components/AccountList.jsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+
 const AccountList = ({ accounts, setAccount }) => {
     const deleteHandler = id => {
         setAccount(prev => prev.filter(acc => acc.id !== id));
+    };
+
+    const [balance, setBalance] = useState(0);
+
+    const sumHandler = e => {
+        setBalance(e.target.value);
+    };
+    const depositHandler = id => {
+        const updatedMoney = accounts.map(acc =>
+            acc.id === id && balance !== 0 ? { ...acc, sum: acc.sum + +balance } : acc
+        );
+        setAccount(updatedMoney);
     };
 
     return (
@@ -13,8 +27,11 @@ const AccountList = ({ accounts, setAccount }) => {
                         <p>{acc.lastName}</p>
                         <p>{acc.sum}</p>
                         <button onClick={() => deleteHandler(acc.id)}>Delete Acc</button>
-                        <input type="number" />
-                        <button>Deposit</button>
+                        <input
+                            type="number"
+                            onChange={sumHandler}
+                        />
+                        <button onClick={() => depositHandler(acc.id)}>Deposit</button>
                         <button>Withdraw</button>
                     </div>
                 ))}
